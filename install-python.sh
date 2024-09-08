5791a0d0c8966c3c0643a0e878a2a6419c2ea229
c <colinmorrison@hotmail.com>
2024-09-08 15:26:37 +0100

    Trying to fix python bootstrap error

--- install-python.sh
+++ install-python.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 sudo apt-get install -y python3-pip
-sudo pip3 install --upgrade pip --break-system-packages
+#sudo pip3 install --upgrade pip --break-system-packages python3 already installed on newer Ubuntu 24.04 LTS version
