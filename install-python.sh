cd4acd5bcdd27f4ce61f3702071d0d60d2d2ed56
c <colinmorrison@hotmail.com>
2024-09-09 18:07:47 +0100

    Fixing pip hvac install

--- install-python.sh
+++ install-python.sh
@@ -4,5 +4,4 @@ sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo apt-get install -y pipx
 pipx ensurepath
-pip install -t /home/azureuser/.local/share/pipx/shared/lib/python3.12/site-packages hvac
 #sudo pip3 install --upgrade pip --break-system-packages python3 already installed on newer Ubuntu 24.04 LTS version
