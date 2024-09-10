bf719da125dd3c05dd6d04c9e4cbfbf74939e886
c <colinmorrison@hotmail.com>
2024-09-10 21:08:20 +0100

    Fixing leaf cert creation issue on Consul hosts

--- install-python.sh
+++ install-python.sh
@@ -4,4 +4,5 @@ sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo apt-get install -y pipx
 pipx ensurepath
+sudo apt install python3-hvac
 #sudo pip3 install --upgrade pip --break-system-packages python3 already installed on newer Ubuntu 24.04 LTS version
